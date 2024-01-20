e94cf7e5281a18d62b747cf7b05f9a86a034cbc5
Luka Chubinidze <chubinidzel08@gmail.com>
2024-01-20 21:07:15 +0400

    add automatication; changing active dot and sliderGroup in every 2.5s

--- script/sliderJS.js
+++ script/sliderJS.js
@@ -1,16 +1,37 @@
-// Bindings to the DOM elements
-const items = Array.from(document.querySelectorAll(".partners_section__slider_group"))
+//// Bindings to the DOM elements
+const groups = Array.from(document.querySelectorAll(".partners_section__slider_group"))
 const dots = Array.from(document.querySelectorAll(".partners_section__dot"))
-
 const leftArrow = document.querySelector(".partners_section__left_arrow")
 const rightArrow = document.querySelector(".partners_section__right_arrow")
 
-dots.forEach(dot => dot.addEventListener("click", () => {
-    dots.forEach(dot => dot.classList.remove("active"))
-    dot.classList.add("active")
+
+//// Manually change script
+
+// functions
+const isActiveDot = (id) => {
+    dots.forEach(dot => dot.classList.remove("active_opacity"))
+    dots[id].classList.add("active_opacity")
+}
+const isShownGroup = (id) => {
+    groups.forEach(group => group.classList.remove("active_shown"))
+    groups[id].classList.add("active_shown")
+}
+
+// Clicking on the dot
+dots.forEach(dot => dot.addEventListener("click", (e) => {
+    isActiveDot(dots.indexOf(e.target))
+    isShownGroup(dots.indexOf(e.target))
 }))
 
-rightArrow.addEventListener("click", () => {
-})
+// Clicking on the arrows
+rightArrow.addEventListener("click", () => console.log("clicked right one"))
+leftArrow.addEventListener("click", () => console.log("clicked left one"))
 
-console.log("");
+
+//// Automatically change script
+let current = 0
+setInterval(() => {
+    current = (current + 1) % 3
+    isShownGroup(current)
+    isActiveDot(current)
+}, 2500);
